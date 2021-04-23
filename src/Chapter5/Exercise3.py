d568450ffe3f640cfb5016c89bd31898ef84094f
group6BCS1 <semiyagijonathan@gmail.com>
2021-04-23 14:55:42 +0300

    chapter5

--- src/Chapter5/Exercise3.py
+++ src/Chapter5/Exercise3.py
@@ -1,3 +1,4 @@
+
 stock = ('''
 25 nickles
 25 dimes
@@ -5,26 +6,57 @@ stock = ('''
 0 ones
 0 fives
 ''')
+def start_start():
+    print(">>> =================RESTART=================\n>>>")
+    print("Welcome to the vending machine change maker program\nChange maker initialized.\nStock contains:")
+    print(stock)
+
+start_start()
 
 
-print(">>> =================RESTART=================\n>>>")
-print("Welcome to the vending machine change maker program\nChange maker initialized.\nStock contains:")
+
 
 
 
 
 deposits = ('''
-    n - deposit a nickel
-    d - deposit a dime
-    q - deposit a quarter
-    o - deposit a one dollar bill
-    f - deposit a five dollar bill
-    c - cancel the purchase
+    enter:
+    n to deposit a nickel
+    d to deposit a dime
+    q to deposit a quarter
+    o to deposit a one dollar bill
+    f to deposit a five dollar bill
+    c to cancel the purchase
 ''')
 
-input = float(input('enter the purchase price in form of xx.xx $' or ' "q" to quit'))
-
-if input == ('enter the purchase price in form of xx.xx $'):
+def enter_price():
+    print('please enter your pay')
     print(deposits)
-else:
-    print(stock)
+
+
+
+try:
+    price = float(input('Enter price in form of xx.xx or q to quit'))
+    enter_price()
+except ValueError:
+    start_start()
+
+
+
+
+c = float(input("please enter an amount to make change: "))
+print('payment due is : ')
+print(c//5, "fives")
+c = c%5
+print(c//1, "ones")
+c = c%1
+print(c//0.25, "quarters")
+c = c%0.25
+print(c//0.1, "dimes")
+c = c%0.1
+print(c//0.05, "nickels")
+
+
+
+
+
