7a432038db99de8b0e3d86c80b8245aa19ad4c56
group17BSE1 <jonathanmarksemiyagi@gmail.com>
2021-05-23 07:54:06 +0100

    final commit

--- src/project_02/project2_a.py
+++ src/project_02/project2_a.py
@@ -1,32 +1,22 @@
 """The program in “project2_a.py” will copy selected lines from “measles.txt” into a
 file selected by the user"""
 try:
-    measles = open("measles.txt", "r")
-except FileExistsError:
-    exit("File can't be to opened.")
-# Prompt for output file name and the year of reference'
-file_name = input("Enter the output file name: ")
-year = input("Enter a year: ")
-
-try:
-    if (year == int(year)) or year == "" or year == "all" or year == "ALL":
+    file = open('measles.txt', 'r')  # open measles.txt in a reading mode.
+    # prompt for the year of reference and output file where to save the text.
+    yr_prompt = input('Enter year of reference>>')
+    file_out = input('Enter file to save data to>>')
+    if '.txt' in file_out:  # if file has the extension skip the else clause and execute the other code lines
         pass
-except ValueError:
-    print("You have entered a wrong year!")  # an error pops up when the condition above is not satisfied
-    exit()
-
-new_file = open(file_name, 'w+')  # This will overwrite the file in case the file name exists already.
-count = 0
-for line in measles:
-    if (line[-5] == year) or (line[-4] == year) or (line[-3] == year) or (line[-1] == year):
-        new_file.write(line)  # This is to add data to the new file created.
-        count += 1
-    elif year == "" or year == "all" or year == "ALL":
-        new_file.write(line)  # add content to the new file
-        count += 1
-
-if count > 0:
-    print("File processing complete you can now check for the file.")
-else:
-    print("There are no records for the year entered!")
-    exit()
+    else:
+        print('Error,output file should have [.txt] extension,re-run!!')  # output file should have an extension
+        exit()
+    file_saved = open(file_out, 'w')
+    for line in file:
+        if (yr_prompt == " ") or (yr_prompt.lower() == "all"):
+            file_saved.write(line)  # This is to add data to the new file created.
+        elif line[88:].startswith(yr_prompt):  # year field begins at index 88 and beyond.
+            file_saved.write(line)  # This is to add data to the new file created.
+    file_saved.close
+    file.close
+except FileExistsError:
+    print("Couldn't open file")
