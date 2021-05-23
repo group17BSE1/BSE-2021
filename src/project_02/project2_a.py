c4d5b205b1eafa8903e54724014f76764492fc9a
group17BSE1 <jonathanmarksemiyagi@gmail.com>
2021-05-23 11:58:07 +0100

    final commit

--- src/project_02/project2_a.py
+++ src/project_02/project2_a.py
@@ -3,8 +3,8 @@ file selected by the user"""
 try:
     file = open('measles.txt', 'r')  # open measles.txt in a reading mode.
     # prompt for the year of reference and output file where to save the text.
+    file_out = (input('Enter file to save data to>>').lower())
     yr_prompt = input('Enter year of reference>>')
-    file_out = input('Enter file to save data to>>')
     if '.txt' in file_out:  # if file has the extension skip the else clause and execute the other code lines
         pass
     else:
@@ -16,7 +16,8 @@ try:
             file_saved.write(line)  # This is to add data to the new file created.
         elif line[88:].startswith(yr_prompt):  # year field begins at index 88 and beyond.
             file_saved.write(line)  # This is to add data to the new file created.
-    file_saved.close
-    file.close
 except FileExistsError:
     print("Couldn't open file")
+
+file_out.close
+file_saved.close
